b06ab56692360457fac4dfd4b2754b97977b1f35
stites <sam@stites.io>
2015-10-11 03:25:26 -0700

    v1 - sufficiency

--- src/CSSR/Sufficiency.hs
+++ src/CSSR/Sufficiency.hs
@@ -6,12 +6,14 @@
 -------------------------------------------------------------------------------
 module CSSR.Sufficiency where
 
+import Data.List (find)
+import Data.Maybe (isJust, fromJust)
 import CSSR.CausalState.State (probability, State)
 import CSSR.CausalState.History (Moment, History)
 import CSSR.Initialization (
     lMax,              -- | default = 5
     significanceLevel, -- | default = 0.05
-    sigma,             -- | is      = []
+    sigma,             -- | is      = [""]
     l,                 -- | is      = 0
     alphabet,          -- | given
     parseTree          -- | given
@@ -20,20 +22,23 @@ import CSSR.Initialization (
 ---------------------------------------------------------------------------
 -- | loop
 --
--- Will iterate through the ParseTree
--- in the causal state. If it does not, it will perform a restricted
--- hypothesis test to see if the history belongs in any existing state.
---
--- If neither test succeeds, we will generate a new state for this history.
+-- Will iterate through the states in a breadth-first manner. As we pass
+-- through each tree node, we construct new states by prefixing characters
+-- from the given alphabet.
 -- ========================================================================
 -- TODO: TCO at a later point
 ---------------------------------------------------------------------------
 loop :: [State] -> Integer -> [State]
-loop all@(s:[])     l | l < lMax = loop (test all (constructedHistory) s alpha) (l+1)
-loop all@(s:states) l | l < lMax = loop (test all (constructedHistory) (expandStates) alpha) (l+1)
+loop all@(state:_) l | l < lMax = let histories = constructFrom state
+--  [['']]                            [['a'],['b'],['c']]
+  in concat [loop (test all history state alpha) (l+1) | history <- histories]
 
 loop all _ = all -- for everything else
 
+constructFrom :: State -> [History]
+constructFrom state = [ a:h | a<-alphabet, h<-state]
+
+alpha = significanceLevel --shorthand
 ---------------------------------------------------------------------------
 -- | test
 --
@@ -51,8 +56,8 @@ test allStates history state alpha = let
     if nullHypothesis
     then state
     else if (isJust someState)
-         then (moveStatesTo state') ++ allStates
-         else (moveStatesTo []    ) ++ allStates
+         then (moveStatesTo fromJust someState) ++ allStates
+         else (moveStatesTo                 []) ++ allStates
   where
     moveStatesTo state'= move history state state'
     hypothesisTest inState = (probability history inState) < (1 - alpha)
@@ -65,7 +70,7 @@ test allStates history state alpha = let
 -- ========================================================================
 -- TODO: re-estimation doesn't seem to fit here - could it be redundant?
 ---------------------------------------------------------------------------
-move :: History -> State -> State -> [State, State]
+move :: History -> State -> State -> [State]
 move x s1 s2 = [s1', s2']
   where
     s1' = filter ((/=) x) s1
