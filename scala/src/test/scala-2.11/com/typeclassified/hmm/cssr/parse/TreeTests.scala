35816f5efa86222e0b713139387b43d615c3267c
stites <sam@stites.io>
2015-12-09 09:56:07 -0800

    format

--- scala/src/test/scala-2.11/com/typeclassified/hmm/cssr/parse/TreeTests.scala
+++ scala/src/test/scala-2.11/com/typeclassified/hmm/cssr/parse/TreeTests.scala
@@ -42,150 +42,151 @@ class TreeTests extends WordSpec with Matchers with ProbablisticAsserts with Bef
 
   "loadHistory" should {
 
-  "generating a single branch for an empty tree" in {
-    var children:ListBuffer[Leaf] = null
-    var leaf:Leaf = null
-    Tree.loadHistory(tree, abc)
+    "generating a single branch for an empty tree" in {
+      var children:ListBuffer[Leaf] = null
+      var leaf:Leaf = null
+      Tree.loadHistory(tree, abc)
 
-    children = tree.root.children
-    children should have size 1
+      children = tree.root.children
+      children should have size 1
 
-    leaf = children.head
-    leaf.observed should equal ("c")
-    leaf.observation should equal ("c".head)
+      leaf = children.head
+      leaf.observed should equal ("c")
+      leaf.observation should equal ("c".head)
 
-    children = children.head.children
-    children should have size 1
+      children = children.head.children
+      children should have size 1
 
-    leaf = children.head
-    leaf.observed should equal ("bc")
-    leaf.observation should equal ("bc".head)
+      leaf = children.head
+      leaf.observed should equal ("bc")
+      leaf.observation should equal ("bc".head)
 
-    children = children.head.children
-    children should have size 1
+      children = children.head.children
+      children should have size 1
 
-    leaf = children.head
-    leaf.observed should equal ("abc")
-    leaf.observation should equal ("abc".head)
-  }
+      leaf = children.head
+      leaf.observed should equal ("abc")
+      leaf.observation should equal ("abc".head)
+    }
 
-  "generating multiple root branches if they do not exist" in {
-    Tree.loadHistory(tree, "bc")
-    Tree.loadHistory(tree, "aa")
+    "generating multiple root branches if they do not exist" in {
+      Tree.loadHistory(tree, "bc")
+      Tree.loadHistory(tree, "aa")
 
-    val children = tree.root.children
-    children should have size 2
+      val children = tree.root.children
+      children should have size 2
 
-    val leaf1 = children(0)
-    assertLeafProperties(leaf1, "c")
-    leaf1.children should have size 1
-    val leaf1Child = leaf1.children.head
-    assertLeafProperties(leaf1Child, "bc")
+      val leaf1 = children(0)
+      assertLeafProperties(leaf1, "c")
+      leaf1.children should have size 1
+      val leaf1Child = leaf1.children.head
+      assertLeafProperties(leaf1Child, "bc")
 
 
-    val leaf2 = children(1)
-    assertLeafProperties(leaf2, "a")
-    leaf2.children should have size 1
-    val leaf2Child = leaf2.children.head
-    assertLeafProperties(leaf2Child, "aa")
+      val leaf2 = children(1)
+      assertLeafProperties(leaf2, "a")
+      leaf2.children should have size 1
+      val leaf2Child = leaf2.children.head
+      assertLeafProperties(leaf2Child, "aa")
 
-    Tree.loadHistory(tree, "ba")
+      Tree.loadHistory(tree, "ba")
 
-    children should have size 2
-    leaf1.children should have size 1
-    leaf2.children should have size 2
-    leaf2Child.children should have size 0
-    leaf1Child.children should have size 0
+      children should have size 2
+      leaf1.children should have size 1
+      leaf2.children should have size 2
+      leaf2Child.children should have size 0
+      leaf1Child.children should have size 0
 
-    val leaf2NewChild = leaf2.children.filter(_ != leaf2Child).head
-    assertLeafProperties(leaf2NewChild, "ba")
-  }
+      val leaf2NewChild = leaf2.children.filter(_ != leaf2Child).head
+      assertLeafProperties(leaf2NewChild, "ba")
+    }
 
-  "updating the distribution of the path to the terminal leaf, if the path does not exist" in {
-    Tree.loadHistory(tree, "cb") // loads "cb", updates
-    val root = tree.root
-    var leafInQuestion = root
-    assertProbabalisticDetails(leafInQuestion, 1, Array(0,1,0))
+    "updating the distribution of the path to the terminal leaf, if the path does not exist" in {
+      Tree.loadHistory(tree, "cb") // loads "cb", updates
+      val root = tree.root
+      var leafInQuestion = root
+      assertProbabalisticDetails(leafInQuestion, 1, Array(0,1,0))
 
-    val rootB = leafInQuestion.children.head
-    leafInQuestion = rootB
-    assertProbabalisticDetails(leafInQuestion, 1, Array(0,0,1))
+      val rootB = leafInQuestion.children.head
+      leafInQuestion = rootB
+      assertProbabalisticDetails(leafInQuestion, 1, Array(0,0,1))
 
-    val rootBC = leafInQuestion.children.head
-    leafInQuestion = rootBC
-    assertProbabalisticDetails(leafInQuestion, 0, Array(0,0,0))
-  }
+      val rootBC = leafInQuestion.children.head
+      leafInQuestion = rootBC
+      assertProbabalisticDetails(leafInQuestion, 0, Array(0,0,0))
+    }
 
-  "updating the distribution of a leaf for it's next-step observations" in {
-    Tree.loadHistory(tree, "cb") // loads "cb", updates
-    val root = tree.root
-    var leafInQuestion = root
-    assertProbabalisticDetails(leafInQuestion, 1, Array(0,1,0))
+    "updating the distribution of a leaf for it's next-step observations" in {
+      Tree.loadHistory(tree, "cb") // loads "cb", updates
+      val root = tree.root
+      var leafInQuestion = root
+      assertProbabalisticDetails(leafInQuestion, 1, Array(0,1,0))
 
-    val rootB = leafInQuestion.children.head
-    leafInQuestion = rootB
-    assertProbabalisticDetails(leafInQuestion, 1, Array(0,0,1))
+      val rootB = leafInQuestion.children.head
+      leafInQuestion = rootB
+      assertProbabalisticDetails(leafInQuestion, 1, Array(0,0,1))
 
-    val rootBC = leafInQuestion.children.head
-    leafInQuestion = rootBC
-    assertProbabalisticDetails(leafInQuestion, 0, Array(0,0,0))
+      val rootBC = leafInQuestion.children.head
+      leafInQuestion = rootBC
+      assertProbabalisticDetails(leafInQuestion, 0, Array(0,0,0))
 
-    Tree.loadHistory(tree, "ab")
-    leafInQuestion = tree.root
-    assertProbabalisticDetails(leafInQuestion, 2, Array(0,2,0))
+      Tree.loadHistory(tree, "ab")
+      leafInQuestion = tree.root
+      assertProbabalisticDetails(leafInQuestion, 2, Array(0,2,0))
 
-    leafInQuestion = rootB
-    assertProbabalisticDetails(leafInQuestion, 2, Array(1,0,1))
+      leafInQuestion = rootB
+      assertProbabalisticDetails(leafInQuestion, 2, Array(1,0,1))
 
-    val rootBA = leafInQuestion.children.head
-    leafInQuestion = rootBA
-    assertProbabalisticDetails(leafInQuestion, 0, Array(0,0,0))
+      val rootBA = leafInQuestion.children.head
+      leafInQuestion = rootBA
+      assertProbabalisticDetails(leafInQuestion, 0, Array(0,0,0))
 
-    Tree.loadHistory(tree, "a")
-    leafInQuestion = root
-    assertProbabalisticDetails(leafInQuestion, 3, Array(1,2,0))
+      Tree.loadHistory(tree, "a")
+      leafInQuestion = root
+      assertProbabalisticDetails(leafInQuestion, 3, Array(1,2,0))
 
-    val rootA = leafInQuestion.children.last
-    leafInQuestion = rootA
-    assertProbabalisticDetails(leafInQuestion, 0, Array(0,0,0))
-  }
+      val rootA = leafInQuestion.children.last
+      leafInQuestion = rootA
+      assertProbabalisticDetails(leafInQuestion, 0, Array(0,0,0))
+    }
 
   }
 
   "loadData" when {
-  "loading sequence 'abc' at lMax of 3 (windows of: abc, ab, bc, a, b, c)" should {
+    "loading sequence 'abc' at lMax of 3 (windows of: abc, ab, bc, a, b, c)" should {
 
-  "load all complete moving windows" in {
-    tree = Tree.loadData(abc.toArray, 3)
+      "load all complete moving windows" in {
+        tree = Tree.loadData(abc.toArray, 3)
 
-    var children:ListBuffer[Leaf] = tree.root.children
-    assertChildrenByExactBatch(children, testMap(abc)(0))
+        var children:ListBuffer[Leaf] = tree.root.children
+        assertChildrenByExactBatch(children, testMap(abc)(0))
 
-    children = children.flatMap(_.children)
-    assertChildrenByExactBatch(children, testMap(abc)(1))
+        children = children.flatMap(_.children)
+        assertChildrenByExactBatch(children, testMap(abc)(1))
 
-    children = children.flatMap(_.children)
-    assertChildrenByExactBatch(children, testMap(abc)(2))
+        children = children.flatMap(_.children)
+        assertChildrenByExactBatch(children, testMap(abc)(2))
 
-    children = children.flatMap(_.children)
-    children should have size 0
-  }
-   }
-    "loading sequence 'abcabc' at lMax of 3 (windows of:  abc, bca, cab, ab, bc, ca, a, b, c" should {
-  "load all combinations" in {
-    tree = Tree.loadData(abcabc.toArray, 3)
+        children = children.flatMap(_.children)
+        children should have size 0
+      }
+    }
+    "loading sequence 'abcabc' at lMax of 3 (windows of:  abc, bca, cab, ab, bc, ca, a, b, c)" should {
+      "load all combinations" in {
+        tree = Tree.loadData(abcabc.toArray, 3)
 
-    var children:ListBuffer[Leaf] = tree.root.children
-    assertChildrenByExactBatch(children, testMap(abcabc)(0))
+        var children:ListBuffer[Leaf] = tree.root.children
+        assertChildrenByExactBatch(children, testMap(abcabc)(0))
 
-    children = children.flatMap(_.children)
-    assertChildrenByExactBatch(children, testMap(abcabc)(1))
+        children = children.flatMap(_.children)
+        assertChildrenByExactBatch(children, testMap(abcabc)(1))
 
-    children = children.flatMap(_.children)
-    assertChildrenByExactBatch(children, testMap(abcabc)(2))
+        children = children.flatMap(_.children)
+        assertChildrenByExactBatch(children, testMap(abcabc)(2))
 
-  }
-      "finished, it should also include lMax+1 probabilities and, thus, also have lMax+1 children: abca, bcab, cabc" in {
+      }
+
+      "include lMax+1 probabilities and, thus, also have lMax+1 children: abca, bcab, cabc" in {
         tree = Tree.loadData(abcabc.toArray, 3)
         var children:ListBuffer[Leaf] = tree.root.children // l1
         children = children.flatMap(_.children) //l2
@@ -193,26 +194,26 @@ class TreeTests extends WordSpec with Matchers with ProbablisticAsserts with Bef
         children = children.flatMap(_.children) //l4
         children should have size 3
       }
-  }
+    }
 
-    "loading sequence 'abcbabcbb' at lMax of 3 (windows of: abc, bcb, cba, bab, cbb, ab, bc, cb, ba, bb, a, b, c " should {
+    "loading sequence 'abcbabcbb' at lMax of 3 (windows of: abc, bcb, cba, bab, cbb, ab, bc, cb, ba, bb, a, b, c)" should {
       "load all combinations" in {
-    tree = Tree.loadData(abcbabcbb.toArray, 3)
+        tree = Tree.loadData(abcbabcbb.toArray, 3)
 
-    var children:ListBuffer[Leaf] = tree.root.children
-    assertChildrenByExactBatch(children, testMap(abcbabcbb)(0))
+        var children:ListBuffer[Leaf] = tree.root.children
+        assertChildrenByExactBatch(children, testMap(abcbabcbb)(0))
 
-    assertChildrenByExactBatch(children.filter(_.observation == 'b').head.children, Seq("ab", "cb", "bb"))
+        assertChildrenByExactBatch(children.filter(_.observation == 'b').head.children, Seq("ab", "cb", "bb"))
 
-    children = children.flatMap(_.children)
-    assertChildrenByExactBatch(children, testMap(abcbabcbb)(1))
+        children = children.flatMap(_.children)
+        assertChildrenByExactBatch(children, testMap(abcbabcbb)(1))
 
-    assertChildrenByExactBatch(children.filter(_.observation == 'b').flatMap(_.children), Seq("abc", "cba", "cbb"))
+        assertChildrenByExactBatch(children.filter(_.observation == 'b').flatMap(_.children), Seq("abc", "cba", "cbb"))
 
-    children = children.flatMap(_.children)
-    assertChildrenByExactBatch(children, testMap(abcbabcbb)(2))
+        children = children.flatMap(_.children)
+        assertChildrenByExactBatch(children, testMap(abcbabcbb)(2))
 
-  }
+      }
       "finished, it should also include lMax+1 probabilities and, thus, also have lMax+1 children: abcb, bcba, cbab, babc, bcbb" in {
         tree = Tree.loadData(abcbabcbb.toArray, 3)
         var children:ListBuffer[Leaf] = tree.root.children // l1
@@ -221,83 +222,81 @@ class TreeTests extends WordSpec with Matchers with ProbablisticAsserts with Bef
         children = children.flatMap(_.children) //l4
         children should have size 5
       }
-  }
+    }
 
-  "not add newlines" in {
-    tree = Tree.loadData((abcabc+"\r\n").toArray, 3)
-    assertChildrenByExactBatch(tree.root.children, testMap(abcabc)(0))
-  }
+    "not add newlines" in {
+      tree = Tree.loadData((abcabc+"\r\n").toArray, 3)
+      assertChildrenByExactBatch(tree.root.children, testMap(abcabc)(0))
+    }
 
-  def assertChildrenByExactBatch(children:ListBuffer[Leaf], expected:Seq[String]) = {
-    children should have size expected.length
-    children.map(_.observed)    should contain theSameElementsAs expected
-    children.map(_.observation) should contain theSameElementsAs expected.map(_.head)
-  }
+    def assertChildrenByExactBatch(children:ListBuffer[Leaf], expected:Seq[String]) = {
+      children should have size expected.length
+      children.map(_.observed)    should contain theSameElementsAs expected
+      children.map(_.observation) should contain theSameElementsAs expected.map(_.head)
+    }
 
-  "be able to return the correct leaf" in {
-    AlphabetHolder.alphabet = Alphabet("01".toCharArray)
-    tree = Tree()
-    tree = Tree.loadData("110111011101110111011101110111011101".toArray, 5)
-    println(tree)
-  }
+    "be able to return the correct leaf" in {
+      AlphabetHolder.alphabet = Alphabet("01".toCharArray)
+      tree = Tree()
+      tree = Tree.loadData("110111011101110111011101110111011101".toArray, 5)
+      println(tree)
+    }
   }
 
   "navigateHistory" should {
+    "be able to return the correct leaf" in {
+      tree = Tree.loadData(abc.toArray, 3)
+      val maybeABC = tree.navigateHistory("abc".toList)
+      maybeABC should not be empty
+      assertLeafProperties(maybeABC.get, "abc")
 
-  "be able to return the correct leaf" in {
-    tree = Tree.loadData(abc.toArray, 3)
-    val maybeABC = tree.navigateHistory("abc".toList)
-    maybeABC should not be empty
-    assertLeafProperties(maybeABC.get, "abc")
+      val notPresentA = tree.navigateHistory("abca".toList)
+      notPresentA shouldBe empty
 
-    val notPresentA = tree.navigateHistory("abca".toList)
-    notPresentA shouldBe empty
+      tree = Tree.loadData(abcbabcbb.toArray, 3)
 
-    tree = Tree.loadData(abcbabcbb.toArray, 3)
+      val maybe_BB = tree.navigateHistory("bb".toList)
+      maybe_BB should not be empty
+      assertLeafProperties(maybe_BB.get, "bb")
 
-    val maybe_BB = tree.navigateHistory("bb".toList)
-    maybe_BB should not be empty
-    assertLeafProperties(maybe_BB.get, "bb")
+      val maybeCBB = tree.navigateHistory("cbb".toList)
+      maybeCBB should not be empty
+      assertLeafProperties(maybeCBB.get, "cbb")
 
-    val maybeCBB = tree.navigateHistory("cbb".toList)
-    maybeCBB should not be empty
-    assertLeafProperties(maybeCBB.get, "cbb")
+      maybe_BB.get.children.map(_.observed) should contain (maybeCBB.get.observed)
 
-    maybe_BB.get.children.map(_.observed) should contain (maybeCBB.get.observed)
+      tree = Tree.loadData("aabbabcab".toArray, 3)
 
-    tree = Tree.loadData("aabbabcab".toArray, 3)
+      val maybe_AB = tree.navigateHistory("ab".toList)
+      maybe_AB should not be empty
+      assertLeafProperties(maybe_AB.get, "ab")
 
-    val maybe_AB = tree.navigateHistory("ab".toList)
-    maybe_AB should not be empty
-    assertLeafProperties(maybe_AB.get, "ab")
+      val maybeAAB = tree.navigateHistory("aab".toList)
+      val maybeBAB = tree.navigateHistory("bab".toList)
+      val maybeCAB = tree.navigateHistory("cab".toList)
 
-    val maybeAAB = tree.navigateHistory("aab".toList)
-    val maybeBAB = tree.navigateHistory("bab".toList)
-    val maybeCAB = tree.navigateHistory("cab".toList)
+      val maybeChildren = List(maybeAAB, maybeBAB, maybeCAB)
 
-    val maybeChildren = List(maybeAAB, maybeBAB, maybeCAB)
+      maybeChildren foreach(_ should not be empty)
 
-    maybeChildren foreach(_ should not be empty)
-
-    maybe_AB.get.children.map(_.observed) should contain theSameElementsAs maybeChildren.map(_.get.observed)
-  }
+      maybe_AB.get.children.map(_.observed) should contain theSameElementsAs maybeChildren.map(_.get.observed)
+    }
   }
   "getDepth" should {
+    "be able to return the correct leaf" in {
+      tree = Tree.loadData(abcabc.toArray, 3)
 
-  "be able to return the correct leaf" in {
-    tree = Tree.loadData(abcabc.toArray, 3)
+      tree.getDepth(0).map(_.observed) should contain only ""
 
-    tree.getDepth(0).map(_.observed) should contain only ""
+      tree.getDepth(1).map(_.observed) should contain theSameElementsAs testMap(abcabc)(0)
 
-    tree.getDepth(1).map(_.observed) should contain theSameElementsAs testMap(abcabc)(0)
+      tree.getDepth(2).map(_.observed) should contain theSameElementsAs testMap(abcabc)(1)
 
-    tree.getDepth(2).map(_.observed) should contain theSameElementsAs testMap(abcabc)(1)
+      tree.getDepth(3).map(_.observed) should contain theSameElementsAs testMap(abcabc)(2)
 
-    tree.getDepth(3).map(_.observed) should contain theSameElementsAs testMap(abcabc)(2)
+      tree.getDepth(4) should have size 3
 
-    tree.getDepth(4) should have size 3
-
-    tree.getDepth(5) should have size 0
-  }
+      tree.getDepth(5) should have size 0
+    }
   }
 }
