744a1ee2e1f219705c5da7fdd39615339ddb8faf
stites <sam@stites.io>
2015-12-09 22:43:16 -0800

    complete - tree scenario

--- scala/src/test/scala-2.11/com/typeclassified/hmm/cssr/parse/TreeScenario.scala
+++ scala/src/test/scala-2.11/com/typeclassified/hmm/cssr/parse/TreeScenario.scala
@@ -6,17 +6,21 @@ import org.scalatest.{WordSpec, BeforeAndAfter, Matchers}
 class TreeScenario extends WordSpec with Matchers with ProbablisticAsserts with LeafAsserts with BeforeAndAfter {
   AlphabetHolder.alphabet = Alphabet("01".toCharArray)
   var tree:Tree = Tree.loadData("110111011101110111011101110111011101".toArray, 5)
+  val (_0, _1, _10, _11, _01) = ("0", "1", "10", "11", "01")
+  val (_110, _011, _111, _101) = ("110", "011", "111", "101")
+  val (_1110, _1011, _0111, _1111, _1101) = ("1110", "1011", "0111", "1111", "1101")
+  val (_01110, _11110, _11011, _10111, _01111, _11111, _11101) = ("01110", "11110", "11011", "10111", "01111", "11111", "11101")
 
   "loading the data" when {
     val node_____r = tree.root
 
     val layer1     = node_____r.children
-    val node____0r = layer1.find(_.observed == "0")
-    val node____1r = layer1.find(_.observed == "1")
+    val node____0r = layer1.find(_.observed == _0)
+    val node____1r = layer1.find(_.observed == _1)
 
     "examining the root layer" should {
       "have correct frequency and distributions" in assertProbabalisticDetails(node_____r, Array(9, 27))
-      "have expected leaf properties" in assertChildrenByExactBatch(layer1, Array("0", "1"))
+      "have expected leaf properties" in assertChildrenByExactBatch(layer1, Array(_0, _1))
     }
 
     "examining the 1st layer" should {
@@ -33,14 +37,14 @@ class TreeScenario extends WordSpec with Matchers with ProbablisticAsserts with 
         assertProbabalisticDetails(node____1r.get, Array(9, 17))
       }
       "have expected leaf properties" in {
-        assertChildrenByExactBatch(node____0r.get.children, Array("10"))
-        assertChildrenByExactBatch(node____1r.get.children, Array("11","01"))
+        assertChildrenByExactBatch(node____0r.get.children, Array(_10))
+        assertChildrenByExactBatch(node____1r.get.children, Array(_11,_01))
       }
     }
     val layer2     = layer1.flatMap(_.children)
-    val node___10r = layer2.find(_.observed == "10")
-    val node___11r = layer2.find(_.observed == "11")
-    val node___01r = layer2.find(_.observed == "01")
+    val node___10r = layer2.find(_.observed == _10)
+    val node___11r = layer2.find(_.observed == _11)
+    val node___01r = layer2.find(_.observed == _01)
 
     "examining the 2nd layer" should {
       "have the correct children" in {
@@ -58,33 +62,111 @@ class TreeScenario extends WordSpec with Matchers with ProbablisticAsserts with 
         assertProbabalisticDetails(node___01r.get, Array(0, 9))
       }
       "have expected leaf properties" in {
-        assertChildrenByExactBatch(node___10r.get.children, Array("110"))
-        assertChildrenByExactBatch(node___11r.get.children, Array("011","111"))
-        assertChildrenByExactBatch(node___01r.get.children, Array("101"))
+        assertChildrenByExactBatch(node___10r.get.children, Array(_110))
+        assertChildrenByExactBatch(node___11r.get.children, Array(_011,_111))
+        assertChildrenByExactBatch(node___01r.get.children, Array(_101))
       }
     }
 
     val layer3     = layer2.flatMap(_.children)
-    val node__110r = layer3.find(_.observed == "110")
-    val node__011r = layer3.find(_.observed == "011")
-    val node__111r = layer3.find(_.observed == "111")
-    val node__101r = layer3.find(_.observed == "101")
+    val node__110r = layer3.find(_.observed == _110)
+    val node__011r = layer3.find(_.observed == _011)
+    val node__111r = layer3.find(_.observed == _111)
+    val node__101r = layer3.find(_.observed == _101)
+
+    "examining the 3rd layer" should {
+      "have the correct children" in {
+        layer3.find(_.observed == "010") shouldBe empty
+        node__110r should not be empty
+        node__101r should not be empty
+        node__011r should not be empty
+        node__111r should not be empty
+        val expected = Array(node__110r.get, node__011r.get, node__111r.get, node__101r.get)
+
+        layer3 should have size expected.length
+        layer3 should contain theSameElementsAs expected
+      }
+      "have each have the correct frequency and distributions" in {
+        assertProbabalisticDetails(node__110r.get, Array(0, 8))
+        assertProbabalisticDetails(node__101r.get, Array(0, 9))
+        assertProbabalisticDetails(node__011r.get, Array(0, 8))
+        assertProbabalisticDetails(node__111r.get, Array(8, 0))
+      }
+      "have expected leaf properties" in {
+        assertChildrenByExactBatch(node__110r.get.children, Array(_1110))
+        assertChildrenByExactBatch(node__101r.get.children, Array(_1101))
+        assertChildrenByExactBatch(node__011r.get.children, Array(_1011))
+        assertChildrenByExactBatch(node__111r.get.children, Array(_0111))
+      }
+    }
 
     val layer4     = layer3.flatMap(_.children)
-    val node_1110r = layer4.find(_.observed == "1110")
-    val node_1011r = layer4.find(_.observed == "1011")
-    val node_0111r = layer4.find(_.observed == "0111")
-    val node_1111r = layer4.find(_.observed == "1111")
-    val node_1101r = layer4.find(_.observed == "1101")
+    val node_1110r = layer4.find(_.observed == _1110)
+    val node_1011r = layer4.find(_.observed == _1011)
+    val node_0111r = layer4.find(_.observed == _0111)
+    val node_1101r = layer4.find(_.observed == _1101)
+
+    "examining the 4th layer" should {
+      "have the correct children" in {
+        Array("0110", "0101", "0011", "1111").foreach(o => {
+          layer4.find(_.observed==o) shouldBe empty
+        })
+        node_1110r should not be empty
+        node_1101r should not be empty
+        node_1011r should not be empty
+        node_0111r should not be empty
+        val expected = Array(node_1110r.get, node_1011r.get, node_0111r.get, node_1101r.get)
+
+        layer4 should have size expected.length
+        layer4 should contain theSameElementsAs expected
+      }
+      "have each have the correct frequency and distributions" in {
+        assertProbabalisticDetails(node_1110r.get, Array(8, 0))
+        assertProbabalisticDetails(node_1101r.get, Array(0, 8))
+        assertProbabalisticDetails(node_1011r.get, Array(0, 8))
+        assertProbabalisticDetails(node_0111r.get, Array(0, 8))
+      }
+      "have expected leaf properties" in {
+        assertChildrenByExactBatch(node_1110r.get.children, Array(_01110))
+        assertChildrenByExactBatch(node_1101r.get.children, Array(_11101))
+        assertChildrenByExactBatch(node_1011r.get.children, Array(_11011))
+        assertChildrenByExactBatch(node_0111r.get.children, Array(_10111))
+      }
+    }
 
     val layer5     = layer4.flatMap(_.children)
-    val node01110r = layer5.find(_.observed == "01110")
-    val node11110r = layer5.find(_.observed == "11110")
-    val node11011r = layer5.find(_.observed == "11011")
-    val node10111r = layer5.find(_.observed == "10111")
-    val node01111r = layer5.find(_.observed == "01111")
-    val node11111r = layer5.find(_.observed == "11111")
-    val node11101r = layer5.find(_.observed == "11101")
+    val node11101r = layer5.find(_.observed == _11101)
+    val node11011r = layer5.find(_.observed == _11011)
+    val node10111r = layer5.find(_.observed == _10111)
+    val node01110r = layer5.find(_.observed == _01110)
+
+    "examining the 5th layer" should {
+      "have the correct children" in {
+        Array("01011", "00111", "01101", "11110").foreach(o => {
+          layer5.find(_.observed==o) shouldBe empty
+        })
+        node11101r should not be empty
+        node11011r should not be empty
+        node10111r should not be empty
+        node01110r should not be empty
+        val expected = Array(node11101r.get, node11011r.get, node10111r.get, node01110r.get)
+
+        layer5 should have size expected.length
+        layer5 should contain theSameElementsAs expected
+      }
+      "have each have the correct frequency and distributions" in {
+        assertProbabalisticDetails(node11101r.get, Array(8,0))
+        assertProbabalisticDetails(node11011r.get, Array(0,7))
+        assertProbabalisticDetails(node10111r.get, Array(0,8))
+        assertProbabalisticDetails(node01110r.get, Array(0,8))
+      }
+      "have expected leaf properties" in {
+        assertChildrenByExactBatch(node11101r.get.children, Array("011101"))
+        assertChildrenByExactBatch(node11011r.get.children, Array("111011"))
+        assertChildrenByExactBatch(node10111r.get.children, Array("110111"))
+        assertChildrenByExactBatch(node01110r.get.children, Array("101110"))
+      }
+    }
 
   }
 }
