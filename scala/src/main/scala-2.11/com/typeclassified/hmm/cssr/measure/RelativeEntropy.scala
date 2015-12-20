b67fd64c41b07475e9d16f0141f72f94fb49e002
stites <sam@stites.io>
2015-12-19 19:34:18 -0800

    add - relative entropy measure

--- scala/src/main/scala-2.11/com/typeclassified/hmm/cssr/measure/RelativeEntropy.scala
+++ scala/src/main/scala-2.11/com/typeclassified/hmm/cssr/measure/RelativeEntropy.scala
@@ -1,16 +1,18 @@
 package com.typeclassified.hmm.cssr.measure
 
 import breeze.linalg.{DenseVector, sum}
+import breeze.numerics.log
 import com.typeclassified.hmm.cssr.parse.{Alphabet, Leaf, Tree}
 import com.typeclassified.hmm.cssr.state.{Machine, EquivalenceClass}
 
 import scala.collection.mutable.ArrayBuffer
 
 object RelativeEntropy {
-
   /**
     * calculates the probability of all the max length strings in the
     * data based on the inferred machine:
+    *
+    * Kullback-Leibler Distance:
     *
     * \begin{equation}
     *    d= \sum_{k} p_k * \log_2 * { p_k \over q_k }
@@ -21,37 +23,54 @@ object RelativeEntropy {
     * @param alphabet
     */
   def kullbackLeiblerDistance (parseTree:Tree, machine:Machine, alphabet: Alphabet): Double = {
-    // We can't begin a substring of length maxLength at the last (maxLength-1)
-    // positions in the data string
-    def getP(leaf: Option[Leaf], h:Leaf):Double = {
-      return leaf.map(_.frequency(alphabet.map(h.observation))).getOrElse(0d)
-    }
-
     // these are our Ps
-    val pDistribution:Array[ArrayBuffer[Double]] = machine.states
-      .map[EquivalenceClass, ArrayBuffer[Double]](s => {
-      return ArrayBuffer[Double](0d)
-      return s.histories.map[Leaf, Double](h => {
-        val mLeaf: Option[Leaf] = parseTree.navigateHistory(h.observed.init.toList)
-        return getP(mLeaf, h)
-      })})
-
-
+    val ps:Array[Array[Leaf]] = machine.states.map(_.histories.toArray)
+    val pStateProbDist:DenseVector[Double] = getProbabilityDistribution(ps, parseTree)
 
     // these are our Qs
-    val nextIsMaxLengthStrings:Array[Leaf] = parseTree.getDepth(parseTree.maxLength - 1)
-    maxLengthStrings.map[Leaf, Double](strElement => {
-      return strElement.frequency
-    })
+    val observedEqClasses:Map[EquivalenceClass, Array[Leaf]] = parseTree.collectLeaves(parseTree.root.children).groupBy(_.currentEquivalenceClass)
+    val qs:Array[Array[Leaf]] = machine.states.map {
+      eq => observedEqClasses(eq).filter(_.observed.length == parseTree.maxLength)
+    }
+    val qStateProbDist:DenseVector[Double] = getProbabilityDistribution(qs, parseTree)
 
-    val stringProbs = DenseVector.zeros[Double](maxLengthStrings.length)
+    sum( pStateProbDist :* (log(pStateProbDist) :/ (log(qStateProbDist) :* math.log(2))) )
+  }
 
+  def getLeafFreqDistributions(leavesByState:Array[Array[Leaf]], tree: Tree): Array[DenseVector[Double]] = {
+    leavesByState.map { s =>
+      new DenseVector( s.map { h =>
+        tree.navigateHistory(h.observed.init.toList) match {
+          case Some(leaf) => leaf.frequency(tree.alphabet.map(h.observation))
+          case None => 0d
+        } } )
+    }
+  }
+
+  def getProbabilityDistribution(leavesByState:Array[Array[Leaf]], tree: Tree):DenseVector[Double] = {
+    val frequencyDistributions:Array[DenseVector[Double]] = getLeafFreqDistributions(leavesByState, tree)
+    val totalCount:Double = frequencyDistributions.foldLeft(0d)((acc, vec)=> acc + sum(vec))
+    val probabilityDistributions:Array[DenseVector[Double]] = frequencyDistributions.map(_/totalCount)
+
+    // ...or maybe these are what we want...
+    val stateFreqDist:DenseVector[Double] = new DenseVector(frequencyDistributions.map(sum(_)))
+    val stateProbDist:DenseVector[Double] = stateFreqDist / totalCount
+
+    stateProbDist
+  }
+
+
+
+
+
+
+  // ============================================================================
+
+  def original (parseTree:Tree, machine:Machine, alphabet: Alphabet): Double = {
 //    HistoryProbablities.calcStringProbs(gArray, parseTree.maxLength, S, alphabet)
 
     var relEntropy:Double = 0
     var logRatio:Double = 0
-
-
 
     /*
     for ((history, i) <- gArray.view.zipWithIndex) {
