5c81d0972f736d7c418d7617a49e7495a50c68fa
stites <sam@stites.io>
2016-01-02 09:00:49 -0800

    document ks tests

--- scala/src/main/scala/com/typeclassified/hmm/cssr/test/Test.scala
+++ scala/src/main/scala/com/typeclassified/hmm/cssr/test/Test.scala
@@ -1,9 +1,11 @@
 package com.typeclassified.hmm.cssr.test
 
+import breeze.linalg.{sum, DenseVector}
 import com.typeclassified.hmm.cssr.parse.Leaf
 import com.typeclassified.hmm.cssr.state.EquivalenceClass
-import com.typeclassified.hmm.cssr.test.hypothesis.KolmogorovSmirnov
+import com.typeclassified.hmm.cssr.test.hypothesis.{KolmogorovSmirnov=>KS}
 import com.typesafe.scalalogging.Logger
+import org.apache.commons.math3.stat.inference.ChiSquareTest
 import org.slf4j.LoggerFactory
 
 import scala.collection.mutable.ListBuffer
@@ -16,7 +18,7 @@ object Test {
            s: EquivalenceClass,
            sig: Double
           ): Unit= {
-    if (nullHypothesis(s, aXt) > sig) {
+    if (nullHypothesis(s, aXt, sig)) {
       if (!s.histories.contains(aXt)) {
         aXt.changeEquivalenceClass(s)
         s.addHistory(aXt)
@@ -34,23 +36,18 @@ object Test {
     }
   }
 
-  def nullHypothesis(s: EquivalenceClass, aXt: Leaf): Double = {
-    KolmogorovSmirnov
-      .test(
-        s.distribution,
-        s.totalCounts,
-        aXt.distribution,
-        aXt.totalCounts)
+  def nullHypothesis(s: EquivalenceClass, aXt: Leaf, sig:Double): Boolean = {
+    KS.kstwo(s.distribution, s.totalCounts, aXt.distribution, aXt.totalCounts) > sig
   }
 
-  def restrictedHypothesesTesting(S: List[EquivalenceClass],
-                                  s: EquivalenceClass,
-                                  ax: Leaf,
-                                  sig: Double
-                                 ): Option[EquivalenceClass] = {
+  def restrictedHypothesesTesting( S: List[EquivalenceClass],
+                                   s: EquivalenceClass,
+                                   ax: Leaf,
+                                   sig: Double )
+  :Option[EquivalenceClass] = {
     val SStar = S.filter(_ ne s)
     for (sStar <- SStar) {
-      if (nullHypothesis(sStar, ax) > sig) {
+      if (nullHypothesis(sStar, ax, sig)) {
         return Option.apply(sStar)
       }
     }
@@ -60,7 +57,6 @@ object Test {
   def move(x: Leaf, from: EquivalenceClass, to: EquivalenceClass): Unit = {
     // ABC -> A
     // (A->) C->B->A
-    //
     x.changeEquivalenceClass(to)
     from.rmHistory(x)
     to.addHistory(x)
--- scala/src/main/scala/com/typeclassified/hmm/cssr/test/hypothesis/KolmogorovSmirnov.scala
+++ scala/src/main/scala/com/typeclassified/hmm/cssr/test/hypothesis/KolmogorovSmirnov.scala
@@ -4,48 +4,122 @@ import breeze.linalg.{DenseVector, max}
 
 object KolmogorovSmirnov {
   // TODO: Look up tables for control values and verification
-  def test(dist1: DenseVector[Double],
-           dist1Total: Double,
-           dist2: DenseVector[Double],
-           dist2Total: Double
+  /** Kolmogorov-Smirnov Hypothesis Test:
+    *
+    * Given an array `data1[1..n1]`, and an array `data2[1..n2]`, this routine
+    * returns the whether or not their K-S statistic passes a hypothesis test
+    * with a significance level of `a`. This tells us whether or not the data
+    * sets are drawn from the same distribution as determined by the KS
+    * distribution.
+    *
+    * @param data1 the first sample's probability distribution
+    * @param n1    the number of observations for data1
+    * @param data2 the second sample's probability distribution
+    * @param n2    the number of observations for data2
+    * @param a     the siginificance level of the test
+    * @return      whether or not the pvalue is greater than the significance
+    */
+  def kstwoTest( data1: DenseVector[Double],
+                 n1: Double,
+                 data2: DenseVector[Double],
+                 n2: Double,
+                 a: Double )
+  : Boolean = kstwo(data1, n1, data2, n2) > a
+
+  /**
+    * Kolmogorov-Smirnov probability (D > observed)
+    *
+    * This routine returns the significance level for the hypothesis that the
+    * data sets are drawn from the same distribution. Small values show that the
+    * cumulative distribution function of `data1` is significantly different
+    * from that of `data2`.
+    *
+    * This function is defined as:
+    *
+    * Probability(D > observed) = Q_{ks}([\sqrt{N_e}+0.12+0.11+\sqrt{N_e}] D])
+    *
+    * where D is the KS statistic and Q_{ks} is the calculation of significance,
+    * defined  by:
+    *
+    * Q_{ks}(\lambda) = 2 * \sum_{j=1,\infinity}(-1)^{j-1} e^{-2j^{2}\lamda^{2}}
+    *
+    * and N_e is the effective number of datapoints, defined by:
+    *
+    * N_e = \div{N_1 * N_2, N_1 + N_2}
+    *
+    * Note from "Numerical Recipies in C":
+    * > The nature of the approximation involved in [Probability(D > observed)]
+    * > is that it becomes asymptotically accurate as the N_e becomes large, but
+    * > is already quite good for Ne ≥ 4, as small a number as one might ever
+    * > actually use.
+    *
+    * @param data1 the first sample's probability distribution
+    * @param n1    the number of observations for data1
+    * @param data2 the second sample's probability distribution
+    * @param n2    the number of observations for data2
+    * @return      p-value, Probability(D > observed)
+    */
+  def kstwo( data1: DenseVector[Double],
+             n1: Double,
+             data2: DenseVector[Double],
+             n2: Double
           ): Double = {
-    assert(dist1.length == dist2.length)
+    // get the square root of the effective number of data points:
+    val en: Double = math.sqrt((n1 * n2) / (n1 + n2))
 
-    // obtain cumulative distributions
-    // [1,2,3] => [(1), (1+2), (1+2+3)]
-    val cdf1 = dist1.scanRight[Double](0)(_ + _)
-    val cdf2 = dist2.scanRight[Double](0)(_ + _)
-
-    // calculate KS statistic - take max difference between 2 values
-    val largestDiff = max((cdf1 - cdf2).map(math.abs))
-
-    val x: Double = (dist1Total * dist2Total) / (dist1Total + dist2Total)
-    val en: Double = math.sqrt(x)
-
-    // calculate significance level
-    return prob((en + 0.12 + (0.11 / en)) * largestDiff)
+    // calculate P(D > observed)
+    probks((en + 0.12 + (0.11 / en)) * ksstatistic(data1, data2))
   }
 
-  def prob(alam: Double): Double = {
-    val a2 = -2.0 * alam * alam
+  /**
+    * calculate the Kolmogorov-Smirnov statistic
+    *
+    * @param data1  sample one's pdf, used to calculate the first ecdf
+    * @param data2  sample two's pdf, used to calculate the second ecdf
+    * @return       the KS statistic: the supremum of the tow calculated ecdfs
+    */
+  def ksstatistic(data1: DenseVector[Double],
+                  data2: DenseVector[Double]): Double = {
+    assert(data1.length == data2.length)
+
+    // calc empirical cumulative distributions. Should have ascending order.
+    val ecdf1 = data1.scanRight[Double](0)(_+_)
+    val ecdf2 = data2.scanRight[Double](0)(_+_)
+
+    max((ecdf1 - ecdf2).map(math.abs))
+  }
+
+  /**
+    * Kolmogorov-Smirnov probability function, Q_{ks}
+    *
+    * Q_{ks}(\lambda) = 2 * \sum_{j=1,\infinity}(-1)^{j-1} e^{-2j^{2}\lamda^{2}}
+    *
+    * This particular calculation assumes that we are working with discreteness
+    * across the natural numbers.
+    *
+    * @param alam stands for "a lambda", I believe
+    * @return     probability or 1.0 if `probks` fails to converge.
+    */
+  def probks(alam: Double): Double = {
     val EPS1 = 0.001
     val EPS2 = 1.0e-8
 
-    var term = 0.0
-    var fac = 2.0
-    var acc = 0.0
-    var termBF = 0.0
+    var (term, fac, sum, termBF) :(Double, Double, Double, Double) =
+        (0.0,  2.0, 0.0,    0.0)
 
+    val a2 = -2.0 * alam * alam
     for (j <- 1 to 100) {
+
       term = fac * math.exp(a2 * j * j)
-      acc += term
-      if ((math.abs(term) <= EPS1 * termBF) || (math.abs(term) <= EPS2 * acc)) {
-        return acc
+      sum += term
+      if ((math.abs(term) <= EPS1 * termBF) || (math.abs(term) <= EPS2 * sum)) {
+        return sum
       }
-      fac = -fac
+      fac = -fac              // Alternating signs in sum.
       termBF = math.abs(term)
     }
 
-    return 1.0
+    1.0 // Get here only by failing to converge.
   }
 }
+
