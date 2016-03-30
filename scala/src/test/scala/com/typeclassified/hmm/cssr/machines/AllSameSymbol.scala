6baaf3acdb98a22bfbc255cc3501ddb7af2e1746
stites <sam@stites.io>
2016-03-30 10:10:29 -0700

    remove unfinished machine generation

--- scala/src/test/scala/com/typeclassified/hmm/cssr/machines/AllSameSymbol.scala
+++
@@ -1,9 +0,0 @@
-package com.typeclassified.hmm.cssr.machines
-
-/**
-  * Program which produces a long data-file consisting of a long repetition of the same symbol, unspaced.
-  * To be used in conjunction with CSSR for state-inference.
-  */
-class AllSameSymbol (val symbol:Char, val repetitions:Int) extends TestMachine {
-  val inputs = scaffoldFiles("allSameSymbol", symbol.toString, (pw) => for (_ <- 0 to repetitions) pw.print(symbol) )
-}
