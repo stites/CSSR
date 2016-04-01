5c54e84b52c277857448c40970fede9cfdb49aaf
stites <sam@stites.io>
2016-03-31 23:46:44 -0700

    add space for all other machines

--- scala/src/test/scala/com/typeclassified/hmm/cssr/parity/v010Machines.scala
+++ scala/src/test/scala/com/typeclassified/hmm/cssr/parity/v010Machines.scala
@@ -24,16 +24,17 @@ class v010Machines extends FunSuite with Matchers {
 
   val DEFAULT_SIG = 0.001
 
-  def dataFile (folder:String, prefix:String): File = {
+  def dataFile(folder: String, prefix: String): File = {
     new File(getClass.getResource(s"/test-machines/$folder/${prefix}_timeseq").toURI)
   }
 
-  def alphabetFile (alphabet:Alphabet): File = {
+  def alphabetFile(alphabet: Alphabet): File = {
     new File(getClass.getResource(s"/alphabets/${alphabet.toString}").toURI)
   }
 
   test("the even process") {
-    val config = new Config(binaryAlphabet, dataFile("even-process", "EP"), 4, DEFAULT_SIG)
+    val data = dataFile("even-process", "EP")
+    val config = new Config(binaryAlphabet, data, 4, DEFAULT_SIG)
     val results = CSSR.run(config)
 
     results.machine.relativeEntropy === 0.000105397d
@@ -45,7 +46,8 @@ class v010Machines extends FunSuite with Matchers {
   }
 
   test("Misiurewics") {
-    val config = new Config(binaryAlphabet, dataFile("misiurewicz", "Misiurewicz"), 4, DEFAULT_SIG)
+    val data = dataFile("misiurewicz", "Misiurewicz")
+    val config = new Config(binaryAlphabet, data, 4, DEFAULT_SIG)
     val results = CSSR.run(config)
 
     results.machine.relativeEntropy === 0d
@@ -57,7 +59,8 @@ class v010Machines extends FunSuite with Matchers {
   }
 
   test("Foulkes") {
-    val config = new Config(binaryAlphabet, dataFile("foulkes", "Foulkes"), 4, 0.01)
+    val data = dataFile("foulkes", "Foulkes")
+    val config = new Config(binaryAlphabet, data, 4, 0.01)
     val results = CSSR.run(config)
 
     results.machine.relativeEntropy === 0d
@@ -67,4 +70,175 @@ class v010Machines extends FunSuite with Matchers {
     results.machine.variation === 0.00585747d
     results.allStates.states should have size 7
   }
+
+  test("alternating-biased-coins") {
+  /*
+    val data = dataFile("alternating-biased-coins", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("biased-coin") {
+    /*
+    val data = dataFile("biased-coin", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("biased-drift-on-ring") {
+    /*
+    val data = dataFile("biased-drift-on-ring", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("flip_per_holmes_isbell") {
+    /*
+    val data = dataFile("flip_per_holmes_isbell", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("hidden-drift") {
+    /*
+    val data = dataFile("hidden-drift", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("hidden-markov") {
+    /*
+    val data = dataFile("hidden-markov", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("noisy-period-two") {
+    /*
+    val data = dataFile("noisy-period-two", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("periodic-with-noise") {
+    /*
+    val data = dataFile("periodic-with-noise", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("random-hidden-markov") {
+    /*
+    val data = dataFile("random-hidden-markov", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("snd") {
+    /*
+    val data = dataFile("snd", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+  test("state-periodic-and-synching") {
+    /*
+    val data = dataFile("state-periodic-and-synching", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
+
+  test("tricky-hidden-markov") {
+    /*
+    val data = dataFile("tricky-hidden-markov", _)
+    val config = new Config(binaryAlphabet, data, _, _)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === _
+    results.machine.relativeEntropyRate === _
+    results.machine.statisticalComplexity === _
+    results.machine.entropyRate === _
+    results.machine.variation === _
+    results.allStates.states should have size _
+    */
+  }
 }
+
