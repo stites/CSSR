8f64f43cb416674ed99595f9c420dbc4000aa334
stites <sam@stites.io>
2016-03-31 23:14:59 -0700

    test Misiurewicz

--- scala/src/test/scala/com/typeclassified/hmm/cssr/parity/v010Machines.scala
+++ scala/src/test/scala/com/typeclassified/hmm/cssr/parity/v010Machines.scala
@@ -22,6 +22,7 @@ class v010Machines extends FunSuite with Matchers {
 
   val binaryAlphabet = alphabetFile(BINARY)
 
+  val DEFAULT_SIG = 0.001
 
   def dataFile (folder:String, prefix:String): File = {
     new File(getClass.getResource(s"/test-machines/$folder/${prefix}_timeseq").toURI)
@@ -32,7 +33,7 @@ class v010Machines extends FunSuite with Matchers {
   }
 
   test("the even process") {
-    val config = new Config(binaryAlphabet, dataFile("even-process", "EP"), 4, 0.001)
+    val config = new Config(binaryAlphabet, dataFile("even-process", "EP"), 4, DEFAULT_SIG)
     val results = CSSR.run(config)
 
     results.machine.relativeEntropy === 0.000105397d
@@ -41,5 +42,17 @@ class v010Machines extends FunSuite with Matchers {
     results.machine.entropyRate === 0.666321d
     results.machine.variation === 0.0108816d
     results.allStates.states should have size 2
+  }
+
+  test("Misiurewics") {
+    val config = new Config(binaryAlphabet, dataFile("misiurewicz", "Misiurewicz"), 4, DEFAULT_SIG)
+    val results = CSSR.run(config)
+
+    results.machine.relativeEntropy === 0d
+    results.machine.relativeEntropyRate === 1.75591e-07d
+    results.machine.statisticalComplexity === 2.78109d
+    results.machine.entropyRate === 0.828277d
+    results.machine.variation === 0.000218154d
+    results.allStates.states should have size 8
   }
 }
