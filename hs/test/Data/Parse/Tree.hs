7f84932055a73d69830df5f5602042bc0414c9ab
stites <sam@stites.io>
2016-10-26 23:47:03 -0400

    more setting bumping

--- hs/test/Data/Parse/Tree.hs
+++ hs/test/Data/Parse/Tree.hs
@@ -9,8 +9,8 @@ spec :: Spec
 spec =
   describe "a parse tree" $ do
     describe "when we encounter the history 110" $ do
-    describe "when we encounter the history 110" $ do
       it "makes 0 the child of the root node" $ do
+
       it "makes 1 the child of  0  (tree has 10)" $ do
       it "makes 1 the child of 10 (tree has 110)" $ do
 
